c559ca87286c26d8f81ace5ef0b032e3be027507
Ilannildo Viana <ilannildoviana12@gmail.com>
2022-10-28 13:18:55 -0300

    Add: config deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 // export const apiUrl ="https://apimonitora.herokuapp.com/api"; // http://apidev.maissaude.bybitx.com
-export const apiUrl = process.env.REACT_APP_API_URL; // http://apidev.maissaude.bybitx.com
+export const apiUrl =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_API_URL_DEV
+    : process.env.REACT_APP_API_URL_PROD; // http://apidev.maissaude.bybitx.com
 export const api = axios.create({
   baseURL: apiUrl,
   headers: {
